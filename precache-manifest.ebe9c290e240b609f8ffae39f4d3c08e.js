aa86605a7d54643efb9a0b8a50384e9411de064b
amanchaurasiaa <aman.ch234@gmail.com>
2023-04-13 12:57:45 +0530

    Updates

---
+++ precache-manifest.ebe9c290e240b609f8ffae39f4d3c08e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ba2dfda9cb92d7426df628ae9d4c79",
+    "url": "/keeper-App/index.html"
+  },
+  {
+    "revision": "140303718ebb369fd3fc",
+    "url": "/keeper-App/static/js/2.e928da4b.chunk.js"
+  },
+  {
+    "revision": "a42454908dab2f3e8bdf",
+    "url": "/keeper-App/static/js/main.95ae536b.chunk.js"
+  },
+  {
+    "revision": "1f18ef2425b62cd82171",
+    "url": "/keeper-App/static/js/runtime-main.2b207ba6.js"
+  }
+]);
